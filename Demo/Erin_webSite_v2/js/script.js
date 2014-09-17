050e7423904fb9aa586b3b121eacdfa7fdd1047c
webber0928 <bird23074035@gmail.com>
2014-09-17 09:16:38 +0800

    套上modernizr js

---
+++ Demo/Erin_webSite_v2/js/script.js
@@ -0,0 +1,14 @@
+/* Author: 
+
+*/
+
+// $('#test').on('mouseenter', function() {
+//         $(this).fadeOut('slow');
+//         $('#test2').fadeIn('slow');
+// });
+    
+// $('#test2').css({left: $('#test').position().left, top: $('#test').position().top})
+//            .on('mouseleave', function() {
+//         $(this).fadeOut('slow');
+//         $('#test').fadeIn('slow');
+// });
